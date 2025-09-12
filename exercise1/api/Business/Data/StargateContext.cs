2618968bbb2cc12ca778303d1cd85495dc48dadb
Jack Olsen <jack.olsen@gmail.com>
2025-09-11 18:04:43 -0700

    Resolve static analysis warnings, messages

--- exercise1/api/Business/Data/StargateContext.cs
+++ exercise1/api/Business/Data/StargateContext.cs
@@ -3,17 +3,13 @@ using System.Data;
 
 namespace StargateAPI.Business.Data
 {
-    public class StargateContext : DbContext
+    public class StargateContext(DbContextOptions<StargateContext> options) 
+        : DbContext(options)
     {
         public IDbConnection Connection => Database.GetDbConnection();
         public DbSet<Person> People { get; set; }
         public DbSet<AstronautDetail> AstronautDetails { get; set; }
         public DbSet<AstronautDuty> AstronautDuties { get; set; }
-
-        public StargateContext(DbContextOptions<StargateContext> options)
-        : base(options)
-        {
-        }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
