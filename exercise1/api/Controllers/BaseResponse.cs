f6946d28427fcc6d77a9bda06d8e072f8cc0ab4a
Jack Olsen <jack.olsen@gmail.com>
2025-09-12 09:19:25 -0700

    Annotate issue for future consideration

--- exercise1/api/Controllers/BaseResponse.cs
+++ exercise1/api/Controllers/BaseResponse.cs
@@ -6,5 +6,6 @@ public class BaseResponse
 {
     public readonly bool Success = true;
     public readonly string Message = "Successful";
+    // TODO: Do we need ResponseCode property in addition to status code on the response?
     public readonly int ResponseCode = (int)HttpStatusCode.OK;
 }
