537d26c8d58a50400bf9bd301f8813eb7d10d97f
Jack Olsen <jack.olsen@gmail.com>
2025-09-11 18:16:06 -0700

    Resolve static analysis warnings, messages

--- exercise1/api/Business/Queries/GetPeople.cs
+++ exercise1/api/Business/Queries/GetPeople.cs
@@ -4,28 +4,27 @@ using StargateAPI.Business.Data;
 using StargateAPI.Business.Dtos;
 using StargateAPI.Controllers;
 
-namespace StargateAPI.Business.Queries
+namespace StargateAPI.Business.Queries;
+
+public class GetPeople : IRequest<GetPeopleResult> { }
+
+public class GetPeopleHandler(StargateContext context) 
+    : IRequestHandler<GetPeople, GetPeopleResult>
 {
-    public class GetPeople : IRequest<GetPeopleResult> { }
+    public readonly StargateContext _context = context;
 
-    public class GetPeopleHandler(StargateContext context) 
-        : IRequestHandler<GetPeople, GetPeopleResult>
+    public async Task<GetPeopleResult> Handle(GetPeople request, CancellationToken cancellationToken)
     {
-        public readonly StargateContext _context = context;
+        // TODO: Use DbContext
+        var people = await _context.Connection.QueryAsync<PersonAstronaut>(
+            sql: $"SELECT a.Id as PersonId, a.Name, b.CurrentRank, b.CurrentDutyTitle, b.CareerStartDate, b.CareerEndDate FROM [Person] a LEFT JOIN [AstronautDetail] b on b.PersonId = a.Id");
 
-        public async Task<GetPeopleResult> Handle(GetPeople request, CancellationToken cancellationToken)
-        {
-            // TODO: Use DbContext
-            var people = await _context.Connection.QueryAsync<PersonAstronaut>(
-                sql: $"SELECT a.Id as PersonId, a.Name, b.CurrentRank, b.CurrentDutyTitle, b.CareerStartDate, b.CareerEndDate FROM [Person] a LEFT JOIN [AstronautDetail] b on b.PersonId = a.Id");
-
-            return new GetPeopleResult(
-                people: [..people]);
-        }
+        return new GetPeopleResult(
+            people: [..people]);
     }
+}
 
-    public class GetPeopleResult(List<PersonAstronaut> people) : BaseResponse
-    {
-        public readonly List<PersonAstronaut> People = people;
-    }
+public class GetPeopleResult(List<PersonAstronaut> people) : BaseResponse
+{
+    public readonly List<PersonAstronaut> People = people;
 }
