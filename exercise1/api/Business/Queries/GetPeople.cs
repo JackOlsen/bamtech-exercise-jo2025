3d21b6732d3c0143362080eb6eedc7d3bf18e819
Jack Olsen <jack.olsen@gmail.com>
2025-09-11 18:14:03 -0700

    Resolve static analysis warnings, messages

--- exercise1/api/Business/Queries/GetPeople.cs
+++ exercise1/api/Business/Queries/GetPeople.cs
@@ -6,35 +6,26 @@ using StargateAPI.Controllers;
 
 namespace StargateAPI.Business.Queries
 {
-    public class GetPeople : IRequest<GetPeopleResult>
-    {
+    public class GetPeople : IRequest<GetPeopleResult> { }
 
-    }
-
-    public class GetPeopleHandler : IRequestHandler<GetPeople, GetPeopleResult>
+    public class GetPeopleHandler(StargateContext context) 
+        : IRequestHandler<GetPeople, GetPeopleResult>
     {
-        public readonly StargateContext _context;
-        public GetPeopleHandler(StargateContext context)
-        {
-            _context = context;
-        }
+        public readonly StargateContext _context = context;
+
         public async Task<GetPeopleResult> Handle(GetPeople request, CancellationToken cancellationToken)
         {
-            var result = new GetPeopleResult();
+            // TODO: Use DbContext
+            var people = await _context.Connection.QueryAsync<PersonAstronaut>(
+                sql: $"SELECT a.Id as PersonId, a.Name, b.CurrentRank, b.CurrentDutyTitle, b.CareerStartDate, b.CareerEndDate FROM [Person] a LEFT JOIN [AstronautDetail] b on b.PersonId = a.Id");
 
-            var query = $"SELECT a.Id as PersonId, a.Name, b.CurrentRank, b.CurrentDutyTitle, b.CareerStartDate, b.CareerEndDate FROM [Person] a LEFT JOIN [AstronautDetail] b on b.PersonId = a.Id";
-
-            var people = await _context.Connection.QueryAsync<PersonAstronaut>(query);
-
-            result.People = people.ToList();
-
-            return result;
+            return new GetPeopleResult(
+                people: [..people]);
         }
     }
 
-    public class GetPeopleResult : BaseResponse
+    public class GetPeopleResult(List<PersonAstronaut> people) : BaseResponse
     {
-        public List<PersonAstronaut> People { get; set; } = new List<PersonAstronaut> { };
-
+        public readonly List<PersonAstronaut> People = people;
     }
 }
