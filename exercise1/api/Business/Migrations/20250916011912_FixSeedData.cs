7357314646257fa701780be3b75762e123fe0001
Jack Olsen <jack.olsen@gmail.com>
2025-09-15 18:19:37 -0700

    Fix seed data

---
+++ exercise1/api/Business/Migrations/20250916011912_FixSeedData.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StargateAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixSeedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AstronautDetail",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CareerStartDate",
+                value: new DateTime(2010, 1, 2, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "AstronautDuty",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DutyStartDate",
+                value: new DateTime(2010, 2, 3, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AstronautDetail",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CareerStartDate",
+                value: new DateTime(2025, 9, 11, 15, 4, 2, 622, DateTimeKind.Local).AddTicks(9373));
+
+            migrationBuilder.UpdateData(
+                table: "AstronautDuty",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DutyStartDate",
+                value: new DateTime(2025, 9, 11, 15, 4, 2, 622, DateTimeKind.Local).AddTicks(9433));
+        }
+    }
+}
